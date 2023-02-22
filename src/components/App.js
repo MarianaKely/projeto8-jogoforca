1ff00c8366bb98a33848c4ee668c20bd11fd03bc
MarianaKely <marianakely.r@gmail.com>
2023-02-22 17:54:02 -0300

    game keyboard implementation and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,56 @@
-function App() {
+import React from 'react'
+import {useState} from "react"
+import Letters from "./Letters"
+
+
+export default  function App() {
+
+  // constants that enable functionalities on the Letters.js - outset//
+
+  const [individualkeyselected, setIndividualkeyselected] = React.useState([])
+  const [informationscreen, SetInformationscreen] = useState([])
+  const [chooseit, setChooseit] = useState([])
+  const [resultanalysis] = useState([])  
+  const [boxoferrors, SetBoxoferrors] = useState(0) 
+  const [positioninthegame, setPositioninthegame] = useState ("")
+  const [chooseyouroption, setChooseyouroption] = useState(true)
+  const [showit, setShowit] = React.useState('')
+  const [disabledkey, setDisabledkey] = useState(true)
+  const [displayfocus, setDisplayfocus] = useState([]) 
+  const [keysonscreen, setKeysonscreen] = React.useState('button-key-inactive')
+  const [gotitright, setGotitright] = React.useState([])
+
+  // constants that enable functionalities on the Letters.js - end//
+
+
   return (
-    <div >
+    // creation configuration of Letters.js - outset //
+    <div className='container'>
+      
+    
+      <Letters
+        individualkeyselected = {individualkeyselected}
+        setIndividualkeyselected = {setIndividualkeyselected}
+        informationscreen = {informationscreen}
+        SetInformationscreen={SetInformationscreen}
+        chooseit = {chooseit}
+        resultanalysis = {resultanalysis}
+        boxoferrors = {boxoferrors}
+        SetBoxoferrors = {SetBoxoferrors}
+        positioninthegame = {positioninthegame}
+        setPositioninthegame = {setPositioninthegame}
+        setChooseyouroption= {setChooseyouroption}
+        showit = {showit}
+        setShowit={setShowit}
+        disabledkey = {disabledkey}
+        setDisabledkey = {setDisabledkey}
+        displayfocus= {displayfocus}
+        keysonscreen ={keysonscreen}
+        setKeysonscreen={setKeysonscreen}
+        gotitright = {gotitright}
+        setGotitright = {setGotitright}>
+      </Letters>
 
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Letters.js
@@ -0,0 +1,93 @@
+
+// page that configures the keyboard letters - outset //
+
+import alfabeto from "../Alphabet"
+
+// properties area for page configuration - outset //
+
+export default function Letters(
+    {
+        individualkeyselected,
+        setIndividualkeyselected,
+        informationscreen,
+        setInformationscreen,
+        chooseit,
+        resultanalysis,
+        boxoferrors,
+        SetBoxoferrors,
+        individualkeysactivated,
+        setPositioninthegame,
+        setChooseyouroption,
+        setShowit,
+        disabledkey,
+        setDisabledkey
+    }) {
+
+// properties area for page configuration - end //        
+
+
+ // area of ​​analysis of attempts/responses and configuration of errors - outset //  
+
+    let otherboxoferrors = boxoferrors;
+    const keyselector = [...individualkeyselected];
+    const wordresultscreen = informationscreen;
+    const result = [...chooseit];
+    resultanalysis = [...wordresultscreen];        
+
+    function chosenletter(alfabeto) {
+
+        if (!individualkeyselected.includes(alfabeto)) {
+            setIndividualkeyselected([...individualkeyselected, alfabeto]);
+
+
+        }
+        resultanalysis = [...wordresultscreen]
+        if (result.includes(alfabeto)) {
+            for (let i = 0; i < result.length; i++) {
+                if (result[i] === alfabeto) {
+                    resultanalysis[i] = alfabeto
+                    setInformationscreen(resultanalysis);
+                }
+            }
+            setShowit(resultanalysis);
+        }
+        else {
+            if(otherboxoferrors <= 5){
+                boxoferrors++
+                SetBoxoferrors(boxoferrors);
+
+    // less than five the game continues with attempts //            
+        }
+    }
+
+
+    }
+
+  // area of ​​analysis of attempts/responses and configuration of errors - end // 
+  
+  // page layout settings -out set //
+
+
+    return (
+
+        <>
+
+            <div className="keyscontainer">
+              <div className="keys">
+                  {alfabeto.map((alfabeto) =>
+                      <button data-test="letter" onClick={() => chosenletter(alfabeto)}disabled={!keyselector.includes(alfabeto)? disabledkey : true}
+                          className={`${individualkeysactivated} ${keyselector.includes(alfabeto) ? "" : ""}`}
+                          key={alfabeto} >
+                          {alfabeto}
+                      </button>)}
+              </div>
+            </div>    
+
+        </>
+
+    )
+// page layout settings -end //
+    
+}
+
+// page that configures the keyboard letters - end //
